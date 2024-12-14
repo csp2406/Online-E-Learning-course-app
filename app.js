31d36dbda634e1cbee0c7100f1562a64530d00cf
csp2406 <csperumalla11@gmail.com>
2024-12-14 13:43:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from "./logo.png";
 import './App.css';
-import CourseInfo from './Components/CourseInfo/CourseInfo';
+import CourseInfo from './Components/CourseInfo';
 
 function App() {
   return (
     <div className="container-fluid">
-      <img src = {logo} className="text-center bg-info p-3" className = "logo"></img>
+      <img src = "https://img.freepik.com/premium-vector/online-learning-logo_203111-7.jpg" className="text-center bg-info p-3 logo" alt="logo"></img>
       {/* <h1 className="text-center bg-info p-3">Online Courses</h1> */}
       <CourseInfo></CourseInfo>
     </div>
